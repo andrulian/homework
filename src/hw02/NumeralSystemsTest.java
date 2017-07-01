caf6f8c4a02288e270183dfcb2994093b4a0463b
andrulian <andvladul@gmail.com>
2017-07-01 18:25:42 +0300

    +1

--- src/hw02/NumeralSystemsTest.java
+++ src/hw02/NumeralSystemsTest.java
@@ -4,7 +4,7 @@ public class NumeralSystemsTest {
     public static void main(String[] args) {
 //        System.out.println(SimpleUnit.assertsEqual(NumeralSystems.intoBinaryString(2), 1);
     }
-}
+} 
 
 //    intoBinaryString();
 //    intoHexString();
