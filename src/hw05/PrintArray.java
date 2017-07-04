0f590d378c2860076c176f946b6dcc42c269c738
andrulian <0___0@rambler.ru>
2017-07-04 23:35:04 +0300

    hw05 raw solution was implemented

--- src/hw05/PrintArray.java
+++ src/hw05/PrintArray.java
@@ -8,8 +8,15 @@ public class PrintArray {
 
         int[] data = ArrayGenerator.generator(10);
 
-        System.out.println(Arrays.toString(data));
+//        System.out.println(Arrays.toString(data));
 
+        printMethod(data);
+        
+    }
 
+    private static void printMethod(int[] data) {
+        for (int elem : data) {
+            System.out.print(elem + " ");
+        }
     }
 }
