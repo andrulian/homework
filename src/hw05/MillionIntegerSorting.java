11099ca9abd9f045ad1fafa503b84f91558226e0
andrulian <andvladul@gmail.com>
2017-07-05 13:49:02 +0300

    hw05 getNumerousRadix metho was added

--- src/hw05/MillionIntegerSorting.java
+++ src/hw05/MillionIntegerSorting.java
@@ -7,12 +7,15 @@ public class MillionIntegerSorting {
     public static void main(String[] args) {
 //        int[] data = ArrayGenerator.generator(1000000);
 
-        int[] data = ArrayGenerator.generator(10);
+//        int[] data = ArrayGenerator.generator(10);
+        int[] data = new int[] {4, 15, 28, 73, 14, 5, 46, 97, 81, 9};
 
         System.out.print("Unsorted: " + Arrays.toString(data));
 
         final long startTime = System.currentTimeMillis();
-        Arrays.sort(data);
+
+//        Arrays.sort(data);
+        MyLousyRadixSort.makeRadixSort(data);
 
         System.out.print("\nSorted:   " + Arrays.toString(data));
         System.out.println("\n\nTime: " + (System.currentTimeMillis() - startTime) + " ms");
--- src/hw05/MyLousyRadixSort.java
+++ src/hw05/MyLousyRadixSort.java
@@ -2,5 +2,66 @@ package hw05;
 
 public class MyLousyRadixSort {
 
+    public static int[] makeRadixSort(int[] data) {
+
+        int numerousRadix = getNumerousRadix(data);
+
+
+        return data;
+    }
+
+    private static int getNumerousRadix(int[] data) {
+        int numerousRadix = 0;
+
+        for (int elem : data) {
+            int len = String.valueOf(elem).length();
+            if (len > numerousRadix) {
+                numerousRadix = len;
+            }
+        }
+        System.out.println("\nnumerousRadix: " + numerousRadix);
+        return numerousRadix;
+    }
+
+
+//    // Largest place for a 32-bit int is the 1 billion's place
+//        for(int place=1; place <= 1000000000; place *= 10){
+//        // Use counting sort at each digit's place
+//        input = countingSort(input, place);
+//    }
+//
+//        return input;
+//}
+//
+//    private static int[] countingSort(int[] input, int place){
+//        int[] out = new int[input.length];
+//
+//        int[] count = new int[10];
+//
+//        for(int i=0; i < input.length; i++){
+//            int digit = getDigit(input[i], place);
+//            count[digit] += 1;
+//        }
+//
+//        for(int i=1; i < count.length; i++){
+//            count[i] += count[i-1];
+//        }
+//
+//        for(int i = input.length-1; i >= 0; i--){
+//            int digit = getDigit(input[i], place);
+//
+//            out[count[digit]-1] = input[i];
+//            count[digit]--;
+//        }
+//
+//        return out;
+//
+//    }
+//
+//    private static int getDigit(int value, int digitPlace){
+//        return ((value/digitPlace ) % 10);
+//    }
+
+
 
 }
