80491198b717ddaf055ba1ee6c87bece3f32a885
lo91n <andvladul@gmail.com>
2017-07-06 12:14:28 +0300

    hw05 +1

--- src/hw05/MillionIntegerSorting.java
+++ src/hw05/MillionIntegerSorting.java
@@ -9,13 +9,13 @@ public class MillionIntegerSorting {
         int[] data0 = ArrayGenerator.generator(1000000,"real");
         int[] data1 = ArrayGenerator.generator(1000000, "natural");
 
-        final long startTime0 = System.currentTimeMillis();
+        final long startTime = System.currentTimeMillis();
         Arrays.sort(data0);
-        System.out.println("\nArrays.sort time: " + (System.currentTimeMillis() - startTime0) + " ms");
+        System.out.println("\nArrays.sort time: " + (System.currentTimeMillis() - startTime) + " ms");
 
-        final long startTime1 = System.currentTimeMillis();
+        startTime= System.currentTimeMillis();
         RadixSort.makeRadixSort(data1);
-        System.out.println("RadixSort time: " + (System.currentTimeMillis() - startTime1) + " ms");
+        System.out.println("RadixSort time: " + (System.currentTimeMillis() - startTime) + " ms");
 
     }
 }
