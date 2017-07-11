ce65c34b62e62280048f6b4507cb0f349f0e68d2
andrulian <andvladul@gmail.com>
2017-07-11 23:50:24 +0300

    hw07 few test was added

--- src/hw07/MyStringLinkedList.java
+++ src/hw07/MyStringLinkedList.java
@@ -2,4 +2,30 @@ package hw07;
 
 public class MyStringLinkedList {
 
+    public static void add(String elem_1) {
+    }
+
+    public static String set(int i, String s) {
+        return "Old string";
+    }
+
+    public static String get(int i) {
+        return "";
+    }
+
+    public static String remove(String str) {
+        return str;
+    }
+
+    public static String remove(int i) {
+        return "";
+    }
+
+    public static int size() {
+        return 100500;
+    }
+
+    public static boolean isEmpty() {
+        return true;
+    }
 }
--- src/hw07/MyStringLinkedListTest.java
+++ src/hw07/MyStringLinkedListTest.java
@@ -5,22 +5,19 @@ import mytestframework.Asserts;
 
 public class MyStringLinkedListTest {
     public static void main(String[] args) {
-//        MyStringLinkedList.add("elem_0");
-//        MyStringLinkedList.add("elem_1");
-//        MyStringLinkedList.add("elem_2");
-//        MyStringLinkedList.add("elem_3");
-//        MyStringLinkedList.add("elem_4");
-//        MyStringLinkedList.add("elem_5");
-//
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.get(0), "elem_0"));
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.get(1000), ""));
-//
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.size(), 2));
-//
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.set(1, "elem_1.1"), "elem_1"));
-//
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.isEmpty(), false));
-//
-//        System.out.println(Asserts.assertEquals(MyStringLinkedList.remove(4), "elem_4"));
+        MyStringLinkedList.add("elem_0");
+        MyStringLinkedList.add("elem_1");
+        MyStringLinkedList.add("elem_2");
+        MyStringLinkedList.add("elem_3");
+        MyStringLinkedList.add("elem_4");
+        MyStringLinkedList.add("elem_5");
+
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.get(0), "elem_0"));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.get(1000), ""));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.size(), 2));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.set(1, "elem_1.1"), "elem_1"));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.isEmpty(), false));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.remove(4), "elem_4"));
+        System.out.println(Asserts.assertEquals(MyStringLinkedList.remove("elem_3"), ""));
     }
 }
