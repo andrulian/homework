c19002bc322da173a537048b1efac88ac7252b2e
andrulian <andvladul@gmail.com>
2017-08-16 18:09:34 +0300

    iteratot was added

--- src/hw10lesson170809/MyArrayDictionary.java
+++ src/hw10lesson170809/MyArrayDictionary.java
@@ -1,14 +1,15 @@
 package hw10lesson170809;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
 
 //    String[] was used instead of Pair class just for a change.
 
-public class MyArrayDictionary {
+public class MyArrayDictionary implements Iterable<String[]>{
     private static final int MAX = 10;
-    
+
     List<String[]>[] data = new List[MAX];
 
     public void put(String key, String value) {
@@ -52,5 +53,53 @@ public class MyArrayDictionary {
         String[] pair = getPair(key);
 
         return pair == null ? null : pair[1];
+    }
+
+    @Override
+    public Iterator<String[]> iterator() {
+        return new Iterator<String[]>() {
+            int currentElement = -1;
+            Iterator<String[]> listIterator = null;
+
+            @Override
+            public boolean hasNext() {
+
+                if (listIterator!= null && listIterator.hasNext()) {
+                    return true;
+                }
+
+                listIterator = null;
+
+                for (++currentElement; listIterator == null && currentElement < data.length; currentElement++) {
+                    List<String[]> list = data[currentElement];
+
+                    if (list == null) {
+                        continue;
+                    }
+
+                    listIterator = list.iterator();
+
+                    if (!listIterator.hasNext()) {continue;}
+
+                    return listIterator.hasNext();
+                }
+
+                return false;
+            }
+
+            @Override
+            public String[] next() {
+                if (!hasNext()) {
+                    throw new IllegalArgumentException();
+                }
+
+                return listIterator.next();
+            }
+
+            @Override
+            public void remove() {
+
+            }
+        };
     }
 }
--- src/hw10lesson170809/MyArrayDictionaryTest.java
+++ src/hw10lesson170809/MyArrayDictionaryTest.java
@@ -1,4 +1,24 @@
 package hw10lesson170809;
 
+import mytestframework.Asserts;
+
 public class MyArrayDictionaryTest {
+    public static void main(String[] args) {
+
+        MyArrayDictionary mad = new MyArrayDictionary();
+
+        mad.put("Frank", "LA");
+        mad.put("Randy", "NY");
+        mad.put("Andrei", "St. Petersburg");
+        mad.put("Jukka", "Imatra");
+
+        mad.put("Andrei", "Zelenogorsk");
+
+        for (String[] str: mad) {
+            System.out.println(str[0] + " lives in " + str[1]);
+        }
+
+//        System.out.println(Asserts.assertEquals(MyArrayDictionary.remove(0), "elem_0"));
+//        System.out.println(Asserts.assertEquals(MyArrayDictionary.);
+    }
 }
