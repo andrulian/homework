42416f81533244c717df4cbe07659cfbc85072a8
andrulian <andvladul@gmail.com>
2017-08-27 17:21:14 +0300

    classes were moved to specific files

---
+++ src/solitaire/DiscardPile.java
@@ -0,0 +1,30 @@
+package solitaire;
+
+class DiscardPile extends CardPile {
+
+    DiscardPile (int x, int y) { super (x, y); }
+
+    public void addCard (Card aCard) {
+        if (! aCard.faceUp())
+            aCard.flip();
+        super.addCard(aCard);
+    }
+
+    public void select (int tx, int ty) {
+        if (empty())
+            return;
+        Card topCard = pop();
+        for (int i = 0; i < 4; i++)
+            if (Solitare.suitPile[i].canTake(topCard)) {
+                Solitare.suitPile[i].addCard(topCard);
+                return;
+            }
+        for (int i = 0; i < 7; i++)
+            if (Solitare.tableau[i].canTake(topCard)) {
+                Solitare.tableau[i].addCard(topCard);
+                return;
+            }
+        // nobody can use it, put it back on our list
+        addCard(topCard);
+    }
+}
