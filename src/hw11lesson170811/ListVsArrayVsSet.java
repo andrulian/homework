a0a0dbc14d1e82e8d3a1245e5bbbfdad4c2faffe
andrulian <andvladul@gmail.com>
2017-09-06 00:06:57 +0300

    hw11 +1

--- src/hw11lesson170811/ListVsArrayVsSet.java
+++ src/hw11lesson170811/ListVsArrayVsSet.java
@@ -2,10 +2,12 @@ package hw11lesson170811;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.List;
 import java.util.TreeSet;
+
 
 public class ListVsArrayVsSet {
     private static List<ElementClass> linkl;
@@ -17,37 +19,53 @@ public class ListVsArrayVsSet {
         arrl = new ArrayList<>();
         set = new TreeSet<>();
 
+        System.out.println("LinkedList has filled with elements in " + timer(linkl) + " ms");
+        System.out.println("ArrayList has filled with elements in " + timer(arrl) + " ms");
+        System.out.println("TreeSet has filled with elements in " + timer(set) + " ms\n");
+
+        // sort
+
+    }
+
+    private static long timer(Collection obj) {
         long startTime = System.currentTimeMillis();
-        fill(linkl);
+        fill(obj);
         long endTime = System.currentTimeMillis();
 
-        System.out.println("LinkedList has filled with elements in " + (endTime - startTime) + " ms");
-
-        startTime = System.currentTimeMillis();
-        fill(arrl);
-        endTime = System.currentTimeMillis();
-
-        System.out.println("ArrayList has filled with elements in " + (endTime - startTime) + " ms");
-
-        startTime = System.currentTimeMillis();
-        fill(set);
-        endTime = System.currentTimeMillis();
-
-        System.out.println("TreeSet has filled with elements in " + (endTime - startTime) + " ms");
+        return endTime - startTime;
     }
 
+//    private static  long timer(List list) {
+//        long startTime = System.currentTimeMillis();
+//        fill(list);
+//        long endTime = System.currentTimeMillis();
+//
+//        return endTime - startTime;
+//    }
+//
+//    private static  long timer(Set set) {
+//        long startTime = System.currentTimeMillis();
+//        fill(set);
+//        long endTime = System.currentTimeMillis();
+//
+//        return endTime - startTime;
+//    }
 
-
-    private static void fill(List list) {
+    private static void fill(Collection obj) {
         for (int i = 0; i < 1_000_000; i++) {
-            list.add(new ElementClass());
+            obj.add(new ElementClass());
         }
     }
 
-    private static void fill(Set set) {
-        for (int i = 0; i < 1_000_000; i++) {
-            set.add(new ElementClass());
-        }
-    }
-
+//    private static void fill(List list) {
+//        for (int i = 0; i < 1_000_000; i++) {
+//            list.add(new ElementClass());
+//        }
+//    }
+//
+//    private static void fill(Set set) {
+//        for (int i = 0; i < 1_000_000; i++) {
+//            set.add(new ElementClass());
+//        }
+//    }
 }
