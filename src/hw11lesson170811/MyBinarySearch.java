868a8caa74eeef2340477b39043885d8292c5c0d
andrulian <andvladul@gmail.com>
2017-09-12 14:58:42 +0300

    binary search test was finished

--- src/hw11lesson170811/MyBinarySearch.java
+++ src/hw11lesson170811/MyBinarySearch.java
@@ -13,35 +13,9 @@ public class MyBinarySearch {
 
             if (res < 0) lo = mid + 1;
             else if (res > 0) hi = mid - 1;
-            else return mid;
+            else return mid + 1;
         }
 
         return -(lo + 1);
-    }
-
-    public static void main(String[] args) {
-        int[] arrL = new int[12];
-
-        for (int i = 0; i < 10; i++) {
-            arrL[i] = i * 2;
-        }
-
-        arrL[10] = 125;
-        arrL[11] = 128;
-
-//        System.out.println("array: " + Arrays.toString(arrL));
-        for (int i = 0; i < arrL.length; i++) {
-            System.out.print(arrL[i] + " pos: " + (i + 1) + " | ");
-        }
-        System.out.println();
-
-        Arrays.sort(arrL);
-
-        System.out.println("Looking for 11: " + binarySearch(arrL, 11));
-        System.out.println("Looking for 9: " + binarySearch(arrL, 9));
-        System.out.println("Looking for -5: " + binarySearch(arrL, -5));
-        System.out.println("Looking for 12: " + binarySearch(arrL, 12));
-        System.out.println("Looking for 122: " + binarySearch(arrL, 122));
-        System.out.println("Looking for 126: " + binarySearch(arrL, 126));
     }
 }
--- src/hw11lesson170811/MyBinarySearchTest.java
+++ src/hw11lesson170811/MyBinarySearchTest.java
@@ -2,23 +2,32 @@ package hw11lesson170811;
 
 import mytestframework.Asserts;
 
-import java.util.Arrays;
-
 public class MyBinarySearchTest {
     public static void main(String[] args) {
 
-        int[] arrL = new int[10];
-
-        for (int i = 0; i < 10; i++) {
-            arrL[i] = i * 2;
-        }
-        System.out.println(Arrays.toString(arrL));
+        int[] arrL = fillArray();
 
         System.out.println(Asserts.assertEquals(MyBinarySearch.binarySearch(arrL, 9), -6));
         System.out.println(Asserts.assertEquals(MyBinarySearch.binarySearch(arrL, -5), -1));
         System.out.println(Asserts.assertEquals(MyBinarySearch.binarySearch(arrL, 12), 7));
         System.out.println(Asserts.assertEquals(MyBinarySearch.binarySearch(arrL, 122), -11));
-
     }
 
+    private static int[] fillArray() {
+        int[] arrL = new int[12];
+
+        for (int i = 0; i < 10; i++) {
+            arrL[i] = i * 2;
+        }
+
+        arrL[10] = 125;
+        arrL[11] = 128;
+
+        for (int i = 0; i < arrL.length; i++) {
+            System.out.print("elm: " + arrL[i] + ", pos: " + (i + 1) + "|");
+        }
+        System.out.println("\n");
+
+        return arrL;
+    }
 }
